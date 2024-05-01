e961254ad61f1b5e251f22e5a723454be636eb12
JackH1402 <159732933+JackH1402@users.noreply.github.com>
2024-05-02 10:29:39 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Ask the user there name
+name = input("What's your name?")
+# Greet the user and introduce the quiz
+print("Welcome to this quiz,", name)
+print("This quiz is about countries")
+# Ask the user a question
+input("What is the smallest country by land mass?")
+# Tell the user the answer
+print("The answer is Vatican City")
+# Ask the user another question
+input("What country has the highest population?")
+# Tell the user the answer
+print("the answer is India with 1.4 billion") 
