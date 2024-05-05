aee81bc4cde0ceaf81583131f586d3d446cd938c
JackH1402 <159732933+JackH1402@users.noreply.github.com>
2024-05-06 11:58:11 +1200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ name = input("What's your name?")
 print("Welcome to this quiz,", name)
 print("This quiz is about countries")
 # Ask the user a question
-input("What is the smallest country by land mass?")
+answer = input("What is the smallest country by land mass?")
 # Tell the user the answer
-print("The answer is Vatican City")
+if answer == "Vatican City":
+  print("Correct!")
+else:
+  print("Wrong!")
+print("The asnwer is Vaitcan City.")
 # Ask the user another question
-input("What country has the highest population?")
+answer = input("What country has the highest population?")
 # Tell the user the answer
-print("the answer is India with 1.4 billion") 
+print("the answer is India with 1.4 billion")
