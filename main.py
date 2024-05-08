8c26731074f346044b296bafbca596180dd7d739
Jack Heileson <heilesonj@wsc.school.nz>
2024-05-09 10:27:16 +1200

    Update main.py

--- main.py
+++ main.py
@@ -9,32 +9,34 @@ else:
 print("Welcome to this quiz,", name)
 print("This quiz is about countries")
 # Ask the user a question
-answer = input("What is the smallest country by land mass?")
+answer = input("What is the smallest country by land mass?").lower()
 # Tell the user the answer
-if answer == "Vatican City":
+if answer == "Vatican City" .lower():
   print("Correct! Good job +1")
   score+= 1
 else:
   print("Wrong! Unlucky -1")
-print("The answer is Vaitcan City.")
-score -= 1
+  print("The answer is Vatican City.")
+  score-= 1
 # Ask the user another question
-answer = input("What country has the highest population?")
+question = ("What country has the highest population?").lower()
 # Tell the user the answer
-print("the answer is India with 1.4 billion")
-if answer == "India":
+if answer == "India" .lower():
   print("Correct! Keep it up +1")
   score+= 2
 else:
   print("Wrong! You got to try harder -1")
+  print("the answer is India with 1.4 billion")
   score-= 2
 # Ask the user another question
-answer = input("What country has the lowest sea level?")
+answer = input("What country has the lowest sea level?").lower()
 # Tell the user the answer
-if answer == "The Maldives":
+if answer == "The Maldives" .lower():
   print("Correct! You are on a roll +1")
   score+=3
 else:
   print("Wrong! You may want to learn Geography -1")
   print("The answer is Maldives at 1.8m or 6ft")
   score-=3
+print("Well done. That's the end. You got ", score, "points")
+print("Well done {}. You finished this quiz. Your final score was {}".format(name, score))
