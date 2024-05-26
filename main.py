78b1c814a6d8439e3e341ce4a622b50f07e36b9a
JackH1402 <159732933+JackH1402@users.noreply.github.com>
2024-05-27 11:59:33 +1200

    Update main.py

--- main.py
+++ main.py
@@ -4,79 +4,37 @@ QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
 GOOD_COMMENTS = ["Good job!", "Keep it up!", "Your on a roll!"]
 BAD_COMMENTS = ["Unlucky", "Keep trying", "You need some help"]
 
+QUESTIONS = ["What is the smallest country by land mass?",
+              "What country has the highest population?",
+                "What country has the lowest sea level?",]
+
+OPTIONS = [["San Marino", "Tonga", "Russia", "Vatican City"],
+            ["Sri Lanka", "India", "China", "France"],
+             ["Qatar", "Denmark", "New Zealand", "Maldives"]]
+SHORT_OPTIONS = ["a", "b", "c", "d"]
+ANSWERS = [3,1,3]
+
 import random
 score = 0
+
 # Ask the user there name
 name = input("What's your name?")
 # Greet the user and introduce the quiz
-if name == "Jack":
-  print("What a silly name!")
-else:
-  print("I don't know that name, who are you?")
 print("Welcome to this quiz,", name)
 print("This quiz is about countries")
 
 
-while play == "yes":
-  while True:
+while True:
     try:
-      tries = input(" How many attempts do you want at each question? 1-4")
+      tries = input("How many attempts do you want at each question? ")
       tries = int(tries)
-      break
+      print("Alright, you will now get {} tries for each question.".format(tries))
     except:
-      print("That's not a number")
-      question_attempts = tries
-      while question_attempts > 0:
-  # Ask the user a question
-  answer = input("What is the smallest country by land mass?").lower()
-  a = "San Marino"
-  b = "Tonga"
-  c = "Russia"
-  d = "Vatican City"
-  # Tell the user the answer
-  if answer == "Vatican City" .lower():
-    print("Correct")
-    print(random.choice(GOOD_COMMENTS))
-    score+= 1
-  else:
-    print("Wrong!")
-    print(random.choice(BAD_COMMENTS))
-    print("The answer is Vatican City.")
-    question_attempts = 1
-    score-= 1
-  # Ask the user another question
-  answer = input("What country has the highest population?").lower()
-  # Tell the user the answer
-  if answer == "India" .lower():
-    print("Correct! Keep it up +1")
-    score+= 2
-  else:
-    print("Wrong! You got to try harder -1")
-    print("the answer is India with 1.4 billion")
-    score-= 2
-  # Ask the user another question
-  question = ("What country has the lowest sea level?").lower()
-  a = "Qatar"
-  b = "Denmark"
-  c = "Maldives"
-  d = "New Zealand"
-  answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
-  # Tell the user the answer
-  if answer == c or answer == "c":
-    print("Correct! You are on a roll +1")
-    score+=3
-  elif answer == "":
-    print("Not sure?")
-  elif answer != a and answer != "a" and answer != b and answer != "b" and answer != d and answer != "d":
-    print("That wasn't an option")
-  else:
-    print("Wrong! You may want to learn Geography -1")
-    print("The answer is Maldives at 1.8m or 6ft")
-    score-=3
-  
-  # End the quiz
-  print("Well done {}. You finished this quiz. Your final score was {}".format(name, score))
-  # Replay
-  play = input("Do you want to play again?").lower()
+      print("That is not a number.")
 
-print("Goodbye")
+
+
+
+
+         
+# Ask the user a question
